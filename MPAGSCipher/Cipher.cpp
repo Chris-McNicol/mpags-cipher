4bd053d44ebf1f6e4337332f5adb94e9d6dce475
Christopher McNicol <phrfwb@rosier.epp.warwick.ac.uk>
2015-11-04 12:30:35 +0000

    fixed bug with inputting from file

--- MPAGSCipher/Cipher.cpp
+++ MPAGSCipher/Cipher.cpp
@@ -1,18 +1,17 @@
 
-
 #include <vector>
-
-
+#include <string>
 
 
 class Cipher{
 
-
-
+  //A vector containing the alphabet
   const std::vector<char> alphabet = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 
 
 public:
+
+  //wrap the index so it won't go out of bounds
   int wrap(int index){
 
     if(index < 0){ index += 26;}
@@ -21,29 +20,32 @@ public:
     return index;
   }
 
+  //returns the character that is shifted
   char shift(char input, int key, bool decrypt){
 
     int index{0};
-
-    
-
+ 
+    //decryption shifts the other way
     if(decrypt) { key = -key;}
 
+    //finds the index of the current char
     for(size_t i =0; i < alphabet.size(); i++){
       if( input == alphabet[i]){
-	index = i;
-	
+	index = i;	
       }
     }
+    
+    //return the char from a shifted position
       return alphabet[ wrap(index + key) ];
   }
 
+
+
+  //applies shift for each char in a string
   std::string apply_cipher(std::string msg, int key, bool decrypt){
 
     std::string output{};
-    //bool success{false};
-    //char temp{'x'};
-
+    
     for(size_t i=0; i < msg.length() ;i++){
       output += shift(msg[i], key, decrypt);
     }
--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -3,7 +3,6 @@
 #include <fstream>
 
 
-//ichange
 // Our program headers
 #include "TransformChar.hpp"
 #include "CommandLineHelpers.hpp"
@@ -17,23 +16,27 @@ void readStream(T& input, S& msg_string){
 }
 
 
+//function to get input text from file
 void get_in_file(std::string location, std::string& msg_string, bool& ok_read){
 
   std::ifstream in_file {location};
-  if (in_file.good()) { ok_read = true;
-    readStream(in_file, msg_string);}
-  
-}
+  if (in_file.good())
+    { ok_read = true;
+      readStream(in_file, msg_string);  }
+  }
 
+//function to request input text from user
 void get_command_text(std::string& msg_string){
   readStream(std::cin, msg_string);}
 
+//function to output string to file
 void put_out_file(std::string location, std::string& msg, bool& ok_write){
   std::ofstream out_file(location);
   out_file << msg ;
   if (out_file.good()) { ok_write = false;}
 }
 
+//function that sets up a cipher and uses it
 std::string CaesarCipher(std::string msg, int key, bool decrypt){
 
   Cipher my_cipher = Cipher();
@@ -41,15 +44,13 @@ std::string CaesarCipher(std::string msg, int key, bool decrypt){
 }
 
 //function to process the Command Line inputs, looking for flags etc.
-bool processCommandLine(const int argc,char* argv[], bool& help, bool& version,bool& in_select, bool& out_select, bool& in_err,bool& out_err,std::string& stuff, std::string& in_file_loc, std::string& out_file_loc, bool& decrypt, int& key, bool& key_select){
+bool processCommandLine(const int argc,char* argv[], bool& help, bool& version,bool& in_select, bool& out_select, bool& in_err,bool& out_err, std::string& in_file_loc, std::string& out_file_loc, bool& decrypt, int& key, bool& key_select){
 
   std::string argument{};
   std::string temp{};
   std::string othertemp{};
 
-  //more fill
-  stuff += "blah";
-
+ 
   for(int arg_check = 1; arg_check < argc; arg_check++){
     argument = argv[arg_check];
 
@@ -124,12 +125,10 @@ int main(int argc, char* argv[]){
   
   std::string msg{};
   std::string input{};
-  std::string stuff{};
+ 
   std::string in_file_loc{};
   std::string out_file_loc{};
   
-  //random fill
-  stuff += "blah";
   
 
   bool help{false};
@@ -145,47 +144,45 @@ int main(int argc, char* argv[]){
   bool key_select{false};
   int key{0};
   
-  bool blah = processCommandLine(argc,argv,help,version,in_select, out_select, in_err,out_err, stuff, in_file_loc, out_file_loc, decrypt, key, key_select);
+  bool blah = processCommandLine(argc,argv,help,version,in_select, out_select, in_err,out_err, in_file_loc, out_file_loc, decrypt, key, key_select);
 
   if(blah) { std::cout << "Command Line entry satisfactory" << std::endl;}
   if(help) { help_called();}
   if(version) {version_called();}
   if(in_err) {input_error_called();}
   if(out_err){output_error_called();}
-
-  if(!in_err && in_select){get_in_file(in_file_loc, input, ok_read);}
-  if(!in_select){
-    char in_char{'x'};
+  
+  if(!(in_err) && in_select){get_in_file(in_file_loc, input, ok_read);}
+    
+  if(!in_select){    
     std::cout << "Text to encrypt, [ENTER] to submit text,  CTRL + D to run program   :  " << std::endl;
-    while(std::cin >> in_char){
-      input += in_char;
-    }
+    get_command_text(input);
   }
-  else{get_command_text(input);}
-  if(ok_write){}
+  
   if(!key_select){std::cout << "No key selected, no encryption performed!" << std::endl;}
     
   //read in characters
   char in_char{'x'};
 
 
-  // msg += stuff;
   
   for(size_t pos=0 ; pos < input.length(); pos++){
-    //while(std::cin >> in_char){
-
+    
     /* Check if character is an alphabetic 
        letter, force it to be uppercase */
+
     in_char = input.at(pos);
     msg += transformChar(in_char); 
        }
 
-
+  //run the Cipher
   std::string encrypted = CaesarCipher(msg, key, decrypt);
 
+  //output the results
   if(out_select){put_out_file(out_file_loc, encrypted, ok_write);}
   else{std::cout << encrypted << std::endl;}
 
+  
   if(ok_write && ok_read){ std::cout << "everything went well" << std::endl;}
 }
 
